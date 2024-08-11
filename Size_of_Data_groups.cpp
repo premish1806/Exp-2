d5ae84109b5b9c81e225ddb0ba53b759dada272c
premish1806 <premishninawe@gmail.com>
2024-08-11 12:32:03 +0530

    Add files via upload

---
+++ Size_of_Data_groups.cpp
@@ -0,0 +1,13 @@
+// Premish Ninawe
+// 23070123092
+// ENTC B1
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    cout << "Size of Integer is: " << sizeof(int) <<" bytes"<< endl;
+    cout << "Size of Float is: " << sizeof(float) <<" bytes"<< endl;
+    cout << "Size of String is: " << sizeof(string) <<" bytes"<< endl;
+    cout << "Size of Character is: " << sizeof(char) <<" bytes"<< endl;
+    cout << "Size of Short Integer is: " << sizeof(short int) <<" bytes"<< endl;
+}
---
+++ Static.cpp
@@ -0,0 +1,17 @@
+// Premish Ninawe
+// 23070123092
+// ENTC B1
+#include<iostream>
+using namespace std;
+
+void staticExample(){
+    int z = 0;
+    z++;
+    cout<<"The value of z is: "<<z<<endl;
+}
+
+int main(){
+    staticExample();
+    staticExample();
+    return 0;
+}
